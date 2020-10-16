4f682e3c6df39a0ab72d211d426ff8aff999f933
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-10-16 21:10:06 +0330

    Add StandardConsoleTable inheriting ConsoleTable library
    The reason is, making some custom configurations in its ctor.

---
+++ include/standard-console-table.hpp
@@ -0,0 +1,24 @@
+#ifndef BEH_TAVAN_STANDARD_CONSOLE_TABLE_HPP
+#define BEH_TAVAN_STANDARD_CONSOLE_TABLE_HPP
+
+#include "CppConsoleTable.hpp"
+
+namespace BehTavan
+{
+    class StandardConsoleTable: public samilton::ConsoleTable
+    {
+        public:
+            StandardConsoleTable();
+
+        protected:
+            /**
+             * Get standard table characters structure.
+             *
+             * As the default one may not be supported in all shells and environments,
+             * instead, this function returns more ASCII characters like + and -.
+             */
+            static TableChars getStandardTableChars() noexcept;
+    };
+}
+
+#endif // BEH_TAVAN_STANDARD_CONSOLE_TABLE_HPP
---
+++ src/standard-console-table.cpp
@@ -0,0 +1,37 @@
+#include "standard-console-table.hpp"
+
+using namespace BehTavan;
+
+using StandardConsoleTable = BehTavan::StandardConsoleTable;
+
+StandardConsoleTable::StandardConsoleTable():
+    samilton::ConsoleTable(
+        // Indents
+        1, 1,
+        // Alignment
+        samilton::Alignment::centre
+    )
+{
+    this->setTableChars(StandardConsoleTable::getStandardTableChars());
+}
+
+StandardConsoleTable::TableChars StandardConsoleTable::getStandardTableChars() noexcept
+{
+    TableChars tableChars;
+
+    tableChars.centreSeparation = '+';
+    tableChars.downSeparation = '+';
+    tableChars.topSeparation = '+';
+    tableChars.leftSeparation = '+';
+    tableChars.rightSeparation = '+';
+
+    tableChars.downRight = '+';
+    tableChars.downLeft = '+';
+    tableChars.topLeft = '+';
+    tableChars.topRight = '+';
+
+    tableChars.leftRightSimple = '|';
+    tableChars.topDownSimple = '-';
+
+    return tableChars;
+}
