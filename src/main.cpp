05ffc679239e2369c34ba9f4bde65dc6740dabfa
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-10-16 21:12:16 +0330

    Complete main.cpp, and update CMakeLists.txt for new libraries
    Now, main() shows a simple table representing the results.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,40 @@
+#include <cstdlib>
+#include <forward_list>
+#include <iostream>
+
+#include "standard-console-table.hpp"
+#include "types.hpp"
+#include "time-measuring.hpp"
+#include "power.hpp"
+
+#include "nameof.hpp"
+
+using namespace BehTavan;
+using namespace BehTavan::TimeMeasuring;
+
 int main()
 {
+    StandardConsoleTable resultTable;
+
+    // The header
+    resultTable.addRow({
+        static_cast<std::string>(""),
+        NAMEOF(power).str(),
+        NAMEOF(powerOptimized).str()
+    });
+
+    std::forward_list<size_t> exponents = {1, 2, 5, 10, 100, 1000, 10000, 100000, 1000000};
+    const BehTavan::Int32 base = 3;
+
+    for (size_t exponent : exponents) {
+        resultTable.addRow({
+            exponent,
+            measureFuncExecTime<TimeUnit::Nanoseconds>(power, base, exponent),
+            measureFuncExecTime<TimeUnit::Nanoseconds>(powerOptimized, base, exponent),
+        });
+    }
+
+    std::cout << resultTable;
+
     return 0;
 }
