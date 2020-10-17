38fafad99995aa778ee2bb6f43a731c334f72158
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-10-17 20:36:17 +0330

    Add a function to get complete list of all power functions info

--- include/power.hpp
+++ include/power.hpp
@@ -1,10 +1,18 @@
 #ifndef BEH_TAVAN_POWER_HPP
 #define BEH_TAVAN_POWER_HPP
 
+#include <vector>
+#include <string>
+
+#include "nameof.hpp"
+
 #include "types.hpp"
 
 namespace BehTavan
 {
+    // All power functions must have the following prototype
+    using PowerFunctionPointer = Int64 (*)(Base, Exponent);
+
     /**
      * Powers an integer to an integer, using a normal algorithm.
      */
@@ -14,6 +22,34 @@ namespace BehTavan
      * Powers an integer to an integer, using the optimized method.
      */
     Int64 powerOptimized(Base base, Exponent exponent);
+
+    /**
+     * A structure to hold properties of a power function.
+     */
+    struct PowerFunctionInfo
+    {
+        PowerFunctionPointer ptr;
+        std::string name;
+    };
+
+    using PowerFunctionInfoList = std::vector<PowerFunctionInfo>;
+
+    /**
+     * Returns all available power functions information.
+     */
+    inline PowerFunctionInfoList getPowerFunctionsInfo()
+    {
+        return {
+            {
+                power,
+                NAMEOF(power).str()
+            },
+            {
+                powerOptimized,
+                NAMEOF(powerOptimized).str()
+            },
+        };
+    }
 }
 
 #endif // BEH_TAVAN_POWER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@
 #include "util.hpp"
 #include "console-table/standard-console-table.hpp"
 
-#include "nameof.hpp"
-
 #define BASE_DEFAULT 3
 #define EXPONENTS_DEFAULT {1, 5, 10, 100, 1000, 10000, 100000, 1000000, 10000000}
 
