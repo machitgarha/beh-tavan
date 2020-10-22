6e8e644daf3d86bbc75dcf48384b60486956ad8b
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-10-22 17:32:18 +0330

    Add a specific overload of getFuncExecTime() for void functions
    The complete description is available in code comments, although
    the reason of this change should be obvious.

--- include/time-measuring.hpp
+++ include/time-measuring.hpp
@@ -3,7 +3,6 @@
 
 #include <cstddef>
 #include <chrono>
-#include <utility>
 #include <stdexcept>
 
 #include "functions/base.hpp"
@@ -44,24 +43,58 @@ namespace BehTavan::TimeMeasuring
         throw new std::invalid_argument("Invalid time unit");
     }
 
+    using _TimePoint = std::chrono::high_resolution_clock::time_point;
+
     /**
-     * A function to measure execution time of a function.
+     * Returns the execution time of a function, for the given input.
      *
-     * @param func The function gets executed.
-     * @param args The arguments forwarded to the function.
+     * @param func The function getting executed.
+     * @param funcOutput Carries the output of the function after its execution. If the
+     * function is void (i.e. returns nothing), then the second overload will be selected,
+     * that do not need this parameter.
+     * @param funcArgs The arguments forwarded to the function.
      * @return How long the execution of the function took, in the specified unit.
      */
     template<typename TimeUnit, typename ReturnType, typename ...ArgTypes>
-    inline Time measureFuncExecTime(
+    inline Time getFuncExecTime(
         const typename FunctionInfo<ReturnType, ArgTypes...>::FunctionType &func,
-        ReturnType &result,
-        ArgTypes ...args
+        ReturnType &funcOutput,
+        ArgTypes &&...funcArgs
     ) {
-        using _TimePoint = std::chrono::high_resolution_clock::time_point;
+        _TimePoint t1, t2;
 
-        _TimePoint t1 = std::chrono::high_resolution_clock::now();
-        result = func(std::forward<ArgTypes>(args)...);
-        _TimePoint t2 = std::chrono::high_resolution_clock::now();
+        t1 = std::chrono::high_resolution_clock::now();
+        funcOutput = func(std::forward<ArgTypes>(funcArgs)...);
+        t2 = std::chrono::high_resolution_clock::now();
+
+        return std::chrono::duration_cast<TimeUnit>(t2 - t1).count();
+    }
+
+    /*
+     * The second overload, only for functions returning void. The reason of creating it
+     * is obvious: void functions do not return anything, so passing a reference to get
+     * its return value is meaningless.
+     *
+     * This and the above version never confuses the compiler, because they differ in the
+     * template parameters count. If you want to be sure, the most possible confusion
+     * should be void function without parameters. In this case, the following version will
+     * be chosen; because, if the first one is selected, then two problems happen: first,
+     * we cannot have a reference to void, and second, the function argument count
+     * mismatch, required 2, but got 1 (i.e. only the function).
+     *
+     * Although compiler can select the proper version correctly from function arguments,
+     * however, it is always good to pass template arguments explicitly.
+     */
+    template<typename TimeUnit, typename ...ArgTypes>
+    inline Time getFuncExecTime(
+        const typename FunctionInfo<void, ArgTypes...>::FunctionType &func,
+        ArgTypes &&...funcArgs
+    ) {
+        _TimePoint t1, t2;
+
+        t1 = std::chrono::high_resolution_clock::now();
+        func(std::forward<ArgTypes>(funcArgs)...);
+        t2 = std::chrono::high_resolution_clock::now();
 
         return std::chrono::duration_cast<TimeUnit>(t2 - t1).count();
     }
