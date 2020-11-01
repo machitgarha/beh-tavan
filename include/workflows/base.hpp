ac55dd6e72eea69962b5a436fe41fa908b937031
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-11-01 18:54:25 +0330

    Initial step to workflows
    Workflows are a set of functions to be run, in order to compare
    some functions and output the results. The benefit is that, every
    workflow can be inherited from another one, and also, they can be
    compound of one or more workflows.

---
+++ include/workflows/base.hpp
@@ -0,0 +1,11 @@
+#ifndef BEH_TAVAN_WORKFLOWS_BASE_HPP
+#define BEH_TAVAN_WORKFLOWS_BASE_HPP
+
+namespace BehTavan::Workflows
+{
+    class BaseWorkflow
+    {
+    };
+}
+
+#endif // BEH_TAVAN_WORKFLOWS_BASE_HPP
---
+++ include/workflows/power.hpp
@@ -0,0 +1,13 @@
+#ifndef BEH_TAVAN_WORKFLOWS_POWER_HPP
+#define BEH_TAVAN_WORKFLOWS_POWER_HPP
+
+#include "base.hpp"
+
+namespace BehTavan::Workflows
+{
+    class PowerWorkflow: public BaseWorkflow
+    {
+    };
+}
+
+#endif // BEH_TAVAN_WORKFLOWS_POWER_HPP
