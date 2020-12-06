74a33e39c3b265b5a080d28e8cedeba95954e5cb
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-12-07 01:36:43 +0330

    Move all power-related jobs into PowerWorkflow class from main

--- include/workflows/power.hpp
+++ include/workflows/power.hpp
@@ -2,11 +2,78 @@
 #define BEH_TAVAN_WORKFLOWS_POWER_HPP
 
 #include "base.hpp"
+#include "input.hpp"
+#include "output.hpp"
+#include "env.hpp"
+#include "types.hpp"
+#include "execution.hpp"
+#include "console-table/execution-result-table.hpp"
+
+
+#include "functions/power.hpp"
+
+#define BASE_DEFAULT 3
+#define EXPONENTS_DEFAULT {1, 5, 10, 100, 1000, 10000, 100000, 1000000, 10000000}
 
 namespace BehTavan::Workflows
 {
+    using namespace Functions;
+    using namespace std;
+
     class PowerWorkflow: public BaseWorkflow
     {
+        public:
+            PowerWorkflow()
+            {
+                const bool interactive = Env::isInteractive();
+
+                const Power::Base &&base = this->getBase(interactive);
+                const Input::Collection<Power::Exponent> &&exponents =
+                    this->getExponents(interactive);
+
+                const auto &funcsInfo = Power::powerFuncsInfo;
+                ExecutionResultTable resultTable("Exponents", funcsInfo);
+
+                using TimeUnit = TimeMeasuring::TimeUnit::Nanoseconds;
+
+                // Print information about the table
+                printVarVal(base);
+                printLine3("Time unit is in ", getPrintableTimeUnit<TimeUnit>(), "s.");
+                printNewLine();
+
+                // Fill the table with data
+                try {
+                    for (Power::Exponent exponent : exponents) {
+                        resultTable.addRow(
+                            exponent,
+                            getFuncExecTimeSet<TimeUnit>(
+                                funcsInfo, std::move(base), std::move(exponent)
+                            )
+                        );
+                    }
+                } catch (std::runtime_error &e) {
+                    printLine2("Runtime error: ", e.what());
+                }
+
+                // Print the table
+                std::cout << resultTable;
+            }
+
+        private:
+            // TODO: Make interactive a class property
+            static inline Power::Base getBase(bool interactive)
+            {
+                return Input::getNumber<Power::Base>(
+                    "base", BASE_DEFAULT, interactive
+                );
+            }
+
+            static inline Input::Collection<Power::Exponent> getExponents(bool interactive)
+            {
+                return Input::getNumberCollection<Power::Exponent>(
+                    "exponent", EXPONENTS_DEFAULT, interactive
+                );
+            }
     };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,13 @@
-#include "env.hpp"
-#include "input.hpp"
 #include "output.hpp"
-#include "types.hpp"
-#include "execution.hpp"
-#include "console-table/execution-result-table.hpp"
-
-#include "functions/power.hpp"
+#include "workflows/power.hpp"
 
 using namespace BehTavan;
-using namespace BehTavan::Functions;
-
-#define BASE_DEFAULT 3
-#define EXPONENTS_DEFAULT {1, 5, 10, 100, 1000, 10000, 100000, 1000000, 10000000}
-
-static inline Power::Base getBase(bool interactive)
-{
-    return Input::getNumber<Power::Base>(
-        "base", BASE_DEFAULT, interactive
-    );
-}
-
-static inline Input::Collection<Power::Exponent> getExponents(bool interactive)
-{
-    return Input::getNumberCollection<Power::Exponent>(
-        "exponent", EXPONENTS_DEFAULT, interactive
-    );
-}
 
 int main()
 {
     Output::printStartMessage();
 
-    const bool interactive = Env::isInteractive();
-
-    const Power::Base &&base = getBase(interactive);
-    const Input::Collection<Power::Exponent> &&exponents = getExponents(interactive);
-
-    const auto &funcsInfo = Power::powerFuncsInfo;
-    ExecutionResultTable resultTable("Exponents", funcsInfo);
-
-    using TimeUnit = TimeMeasuring::TimeUnit::Nanoseconds;
-
-    // Print information about the table
-    printVarVal(base);
-    printLine3("Time unit is in ", getPrintableTimeUnit<TimeUnit>(), "s.");
-    printNewLine();
-
-    // Fill the table with data
-    try {
-        for (Power::Exponent exponent : exponents) {
-            resultTable.addRow(
-                exponent,
-                getFuncExecTimeSet<TimeUnit>(
-                    funcsInfo, std::move(base), std::move(exponent)
-                )
-            );
-        }
-    } catch (std::runtime_error &e) {
-        printLine2("Runtime error: ", e.what());
-    }
-
-    // Print the table
-    std::cout << resultTable;
+    Workflows::PowerWorkflow x;
 
     return 0;
 }
