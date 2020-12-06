b27e228a50f3e504e320af52ef916685caa88731
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-12-07 01:47:40 +0330

    Add BaseWorkflow::run() (abstract) and add comments

--- include/workflows/base.hpp
+++ include/workflows/base.hpp
@@ -3,8 +3,24 @@
 
 namespace BehTavan::Workflows
 {
+    /**
+     * Workflow base class.
+     *
+     * A workflow is a set of works (i.e. functions) done in order to make a whole work
+     * done. Useful to keep things as simple as possible, and also to hide implementation.
+     */
     class BaseWorkflow
     {
+        /**
+         * Runs the workflow in the specified order.
+         *
+         * There should be no work under constructor, it should only prepare necessary
+         * stuff before running the workflow.
+         *
+         * To keep things simple, implementation of this function should be minimal and
+         * restricted to a set of function calls only. More modular, the better.
+         */
+        virtual void run() = 0;
     };
 }
 
