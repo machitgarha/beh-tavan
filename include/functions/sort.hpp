f3f9de3361e7e8670187f0f700bdc96877a1561e
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-12-07 22:09:37 +0330

    Add sorting basic files (currently empty)

---
+++ include/functions/sort.hpp
@@ -0,0 +1,12 @@
+#ifndef BEH_TAVAN_SORT_HPP
+#define BEH_TAVAN_SORT_HPP
+
+#include "sorting/bubble-sort.hpp"
+#include "sorting/counting-sort.hpp"
+#include "sorting/heap-sort.hpp"
+#include "sorting/insertion-sort.hpp"
+#include "sorting/merge-sort.hpp"
+#include "sorting/quick-sort.hpp"
+#include "sorting/radix-sort.hpp"
+
+#endif // BEH_TAVAN_SORT_HPP
