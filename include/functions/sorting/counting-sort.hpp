0c57c49c2332116ca5c14192a5072c7b245389e4
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-12-08 00:29:34 +0330

    Implement counting sort

---
+++ include/functions/sorting/counting-sort.hpp
@@ -0,0 +1,12 @@
+#ifndef BEH_TAVAN_SORTING_COUNTING_SORT_HPP
+#define BEH_TAVAN_SORTING_COUNTING_SORT_HPP
+
+#include <vector>
+
+namespace BehTavan::Functions::Sorting
+{
+    template<typename T>
+    void countingSort(std::vector<T> &);
+}
+
+#endif // BEH_TAVAN_SORTING_COUNTING_SORT_HPP
---
+++ src/functions/sorting/counting-sort.cpp
@@ -0,0 +1,33 @@
+#include "functions/sorting/counting-sort.hpp"
+
+#include <algorithm>
+
+using namespace BehTavan::Functions::Sorting;
+
+template<typename T>
+void countingSort(std::vector<T> &arr)
+{
+    if (arr.empty()) {
+        return;
+    }
+
+    const size_t size = arr.size();
+    T maxElement = *std::max_element(arr.cbegin(), arr.cend());
+
+    std::vector<size_t> counts(maxElement + 1, 0);
+    for (size_t i = 0; i < size; i++) {
+        counts[arr[i]]++;
+    }
+
+    for (size_t i = 1; i < maxElement + 1; i++) {
+        counts[i] = counts[i - 1];
+    }
+
+    std::vector<T> result(size);
+    for (size_t i = 0; i < size; i++) {
+        result[counts[arr[i]] - 1] = arr[i];
+        counts[arr[i]]--;
+    }
+
+    swap(arr, result);
+}
