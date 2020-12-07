f22cf1761c7fadfa5e58e878342d055815b1db7c
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-12-07 23:38:43 +0330

    Implement bubble sort

---
+++ include/functions/sorting/bubble-sort.hpp
@@ -0,0 +1,12 @@
+#ifndef BEH_TAVAN_SORTING_BUBBLE_SORT_HPP
+#define BEH_TAVAN_SORTING_BUBBLE_SORT_HPP
+
+#include <vector>
+
+namespace BehTavan::Functions::Sorting
+{
+    template<typename T>
+    void bubbleSort(std::vector<T> &);
+}
+
+#endif // BEH_TAVAN_SORTING_BUBBLE_SORT_HPP
