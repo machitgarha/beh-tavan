68726f98bff7bec4646f9595c2019fef8ded6203
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-12-08 13:02:16 +0330

    Remove Farsang from the project
    Although it is a good library for PRNG, however, it is too big for
    this project (as it is not only Farsang, but also GSL which is
    huge). So, the default random number generation in C++ will be used
    instead.

---
+++ src/functions/sorting/quick-sort.cpp
@@ -0,0 +1,9 @@
+#include "functions/sorting.hpp"
+
+using namespace BehTavan::Functions;
+
+template<typename T>
+void Sorting::quickSort(std::vector<T> &arr)
+{
+    
+}
--- src/workflows/sorting-time-comparison-workflow.cpp
+++ src/workflows/sorting-time-comparison-workflow.cpp
@@ -1,18 +1,13 @@
 #include "workflows/sorting-time-comparison-workflow.hpp"
 
-#include "random-generator.hpp"
-
 using namespace BehTavan::Workflows;
 
 void SortingTimeComparisonWorkflow::run()
 {
-    size_t n = this->getArraySize();
+    const size_t n = this->getArraySize();
 
     std::vector<Types::UInt64> arr(n);
-    Farsang::RandomGenerator randGen(time(nullptr));
-    for (Types::UInt64 &i : arr) {
-        i = randGen.generate();
-    }
+    // RNG
 
     const CurFunctionInfoVector &funcsInfo = this->getFunctionsInfo();
     // Measure
