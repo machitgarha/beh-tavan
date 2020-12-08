022ad5d7f3b1d64bf33abbf3bd8636cda4622fc9
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2020-12-08 05:11:17 +0330

    Add SortingTimeComparisonWorkflow (partially)

---
+++ include/workflows/sorting-time-comparison-workflow.hpp
@@ -0,0 +1,34 @@
+#ifndef BEH_TAVAN_WORKFLOWS_SORTING_TIME_COMPARISON_HPP
+#define BEH_TAVAN_WORKFLOWS_SORTING_TIME_COMPARISON_HPP
+
+#include "abstract-time-comparison-workflow.hpp"
+
+#include "types.hpp"
+#include "input.hpp"
+#include "nameof.hpp"
+#include "functions/sorting.hpp"
+
+namespace BehTavan::Workflows
+{
+    using namespace Functions;
+
+    class SortingTimeComparisonWorkflow: public AbstractTimeComparisonWorkflow<
+        void,
+        std::vector<Types::UInt64>
+    > {
+        public:
+            virtual void run();
+
+        protected:
+            const size_t defaultArraySize = 256;
+
+            inline size_t getArraySize() const
+            {
+                return Input::getNumber(
+                    "array size", this->defaultArraySize, this->isInteractive
+                );
+            }
+    };
+}
+
+#endif // BEH_TAVAN_WORKFLOWS_SORTING_TIME_COMPARISON_HPP
